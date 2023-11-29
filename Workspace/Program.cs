a5f961c4f935c108b50fe63ce111c166fe7381cf
Álvaro Maciel Cabral Dantas <142815803+AlvaroMaciel93@users.noreply.github.com>
2023-11-29 10:45:53 -0300

    Commit 1: criação das classes Médico e Paciente
    Commit obrigatório solicitado pelo professor

---
+++ Workspace/Program.cs
@@ -0,0 +1,26 @@
+﻿#region POGDEV
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+//Sistema de Gerenciamento de Consultório
+
+public class Program {
+    public static void Main(string[] args){
+        class Medico {
+            public string Nome { get; set; }
+            public string DataNascimento { get; set; }
+            public int CPF { get; set; }
+
+            public string CRM { get; set; }
+        }
+        class Paciente {
+            public string Nome { get; set; }
+            public string DataNascimento { get; set; }
+            public string CPF { get; set; }
+            public string Sexo { get; set; }
+            public string Sintomas { get; set; }
+        }
+    }
+}
