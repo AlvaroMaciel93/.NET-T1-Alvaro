24866de67851183a704fc168cc618c4882783f3e
Álvaro Maciel Cabral Dantas <142815803+AlvaroMaciel93@users.noreply.github.com>
2023-11-29 11:17:25 -0300

    Commit 3: correção de tipo de atributo, finalização de adição de pacientes e início da construção de LINQ para os relatórios
    Commit 3: correção de tipo de atributo, finalização de adição de pacientes e início da construção de LINQ para os relatórios

--- Workspace/Program.cs
+++ Workspace/Program.cs
@@ -17,7 +17,7 @@ public class Program {
         class Paciente {
             public string Nome { get; set; }
             public string DataNascimento { get; set; }
-            public string CPF { get; set; }
+            public int CPF { get; set; }
             public string Sexo { get; set; }
             public string Sintomas { get; set; }
         }
@@ -26,13 +26,11 @@ public class Program {
         List<Paciente> pacientes = new List<Paciente>();
 
         // Métodos para validação de CPF único
-        bool ValidarCPFMedico(string cpf)
-        {
+        bool ValidarCPFMedico(int cpf) {
             return !medicos.Any(m => m.CPF == cpf);
         }
 
-        bool ValidarCPFPaciente(string cpf)
-        {
+        bool ValidarCPFPaciente(int cpf) {
             return !pacientes.Any(p => p.CPF == cpf);
         }
 
@@ -42,14 +40,37 @@ public class Program {
                 medicos.Add(new Medico {
                     Nome = nome, DataNascimento = dataNascimento, CPF = cpf, CRM = crm});
                     Console.WriteLine("Médico adicionado com sucesso!");
-                }
+            }
             else {
                 throw new Exception("CPF já cadastrado para outro médico.");
             }
         }
         //Método para cadastrar paciente
-        void AdicionarPaciente(string nome, string datadenasc, int cpf, string sexo, string sintomas)
-
+        void AdicionarPaciente(string nome, string datadenasc, int cpf, string sexo, string sintomas) {
+            if (ValidarCPFMedico (cpf)){
+                pacientes.Add(new Paciente {
+                    Nome = nome, DataNascimento = datadenasc, CPF = cpf, Sexo = sexo, Sintomas = sintomas});
+                    Console.WriteLine("Paciente adicionado com sucesso!");
+                }
+            else {
+                throw new Exception("CPF já cadastrado para outro paciente.");
+            }
+        }
+        // Consultas LINQ para relatórios
+        void RelatorioMedicosEntreIdades(List<Medico> medicos, int idadeMinima, int idadeMaxima)
+        {
+            var medicosEntreIdades = medicos.Where(m => (DateTime.Now.Year - m.DataNascimento.Year) >= idadeMinima && (DateTime.Now.Year - m.DataNascimento.Year) <= idadeMaxima).ToList();
+            foreach (var medico in medicosEntreIdades)
+            {
+                Console.WriteLine($"Médico: {medico.Nome}, Idade: {DateTime.Now.Year - medico.DataNascimento.Year}");
+            }
+        }
+        void RelatorioPacientesEntreIdades(List<Paciente> pacientes, int idadeMinima, int idadeMaxima)
+        {
+            var pacientesEntreIdades = pacientes.Where(m => (DateTime.Now.Year - m.DataNascimento.Year) >= idadeMinima && (DateTime.Now.Year - m.DataNascimento.Year) <= idadeMaxima).ToList();
+            foreach (var paciente in pacientesEntreIdades)
+            {
+                Console.WriteLine($"Paciente: {paciente.Nome}, Idade: {DateTime.Now.Year - paciente.DataNascimento.Year}");
             }
         }
     }
