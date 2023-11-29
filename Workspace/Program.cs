600823b63ad2c3d493d96c8e9c85e34a77678c9d
Álvaro Maciel Cabral Dantas <142815803+AlvaroMaciel93@users.noreply.github.com>
2023-11-29 10:59:31 -0300

    Commit 2: criação de coleções e validações

--- Workspace/Program.cs
+++ Workspace/Program.cs
@@ -1,4 +1,4 @@
-﻿#region POGDEV
+#region POGDEV
 
 using System;
 using System.Collections.Generic;
@@ -12,7 +12,6 @@ public class Program {
             public string Nome { get; set; }
             public string DataNascimento { get; set; }
             public int CPF { get; set; }
-
             public string CRM { get; set; }
         }
         class Paciente {
@@ -21,6 +20,37 @@ public class Program {
             public string CPF { get; set; }
             public string Sexo { get; set; }
             public string Sintomas { get; set; }
+        }
+         // Coleções para armazenar Médicos e Pacientes
+        List<Medico> medicos = new List<Medico>();
+        List<Paciente> pacientes = new List<Paciente>();
+
+        // Métodos para validação de CPF único
+        bool ValidarCPFMedico(string cpf)
+        {
+            return !medicos.Any(m => m.CPF == cpf);
+        }
+
+        bool ValidarCPFPaciente(string cpf)
+        {
+            return !pacientes.Any(p => p.CPF == cpf);
+        }
+
+        //Método para cadastrar médico
+        void AdicionarMedico(string nome, string dataNascimento, int cpf, string crm) {
+            if (ValidarCPFMedico (cpf)){
+                medicos.Add(new Medico {
+                    Nome = nome, DataNascimento = dataNascimento, CPF = cpf, CRM = crm});
+                    Console.WriteLine("Médico adicionado com sucesso!");
+                }
+            else {
+                throw new Exception("CPF já cadastrado para outro médico.");
+            }
+        }
+        //Método para cadastrar paciente
+        void AdicionarPaciente(string nome, string datadenasc, int cpf, string sexo, string sintomas)
+
+            }
         }
     }
 }
