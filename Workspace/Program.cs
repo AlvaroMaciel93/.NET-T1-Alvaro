bccf1776efffe2403e520e1e905383e2305dcf80
Álvaro Maciel Cabral Dantas <142815803+AlvaroMaciel93@users.noreply.github.com>
2023-11-29 12:40:35 -0300

    Commit final: correção e finalização

--- Workspace/Program.cs
+++ Workspace/Program.cs
@@ -1,106 +1,135 @@
 #region POGDEV
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
-//Sistema de Gerenciamento de Consultório
+class Program
+{
+    class Medico
+    {
+        public string Nome { get; set; }
+        public string DataNascimento { get; set; }
+        public int CPF { get; set; }
+        public string CRM { get; set; }
+    }
 
-public class Program {
-    public static void Main(string[] args){
-        class Medico {
-            public string Nome { get; set; }
-            public string DataNascimento { get; set; }
-            public int CPF { get; set; }
-            public string CRM { get; set; }
-        }
-        class Paciente {
-            public string Nome { get; set; }
-            public string DataNascimento { get; set; }
-            public int CPF { get; set; }
-            public string Sexo { get; set; }
-            public string Sintomas { get; set; }
-        }
-         // Coleções para armazenar Médicos e Pacientes
-        List<Medico> medicos = new List<Medico>();
-        List<Paciente> pacientes = new List<Paciente>();
+    class Paciente
+    {
+        public string Nome { get; set; }
+        public string DataNascimento { get; set; }
+        public int CPF { get; set; }
+        public string Sexo { get; set; }
+        public string Sintomas { get; set; }
+    }
 
-        // Métodos para validação de CPF único
-        bool ValidarCPFMedico(int cpf) {
-            return !medicos.Any(m => m.CPF == cpf);
-        }
+    static List<Medico> medicos = new List<Medico>();
+    static List<Paciente> pacientes = new List<Paciente>();
 
-        bool ValidarCPFPaciente(int cpf) {
-            return !pacientes.Any(p => p.CPF == cpf);
-        }
+    static bool ValidarCPFMedico(int cpf)
+    {
+        return !medicos.Any(m => m.CPF == cpf);
+    }
 
-        //Método para cadastrar médico
-        void AdicionarMedico(string nome, string dataNascimento, int cpf, string crm) {
-            if (ValidarCPFMedico (cpf)){
-                medicos.Add(new Medico {
-                    Nome = nome, DataNascimento = dataNascimento, CPF = cpf, CRM = crm});
-                    Console.WriteLine("Médico adicionado com sucesso!");
-            }
-            else {
-                throw new Exception("CPF já cadastrado para outro médico.");
-            }
-        }
-        //Método para cadastrar paciente
-        void AdicionarPaciente(string nome, string datadenasc, int cpf, string sexo, string sintomas) {
-            if (ValidarCPFMedico (cpf)){
-                pacientes.Add(new Paciente {
-                    Nome = nome, DataNascimento = datadenasc, CPF = cpf, Sexo = sexo, Sintomas = sintomas});
-                    Console.WriteLine("Paciente adicionado com sucesso!");
-                }
-            else {
-                throw new Exception("CPF já cadastrado para outro paciente.");
-            }
-        }
-        // Consultas LINQ para relatórios
-        void RelatorioMedicosEntreIdades(List<Medico> medicos, int idadeMinima, int idadeMaxima) {
-            var medicosEntreIdades = medicos.Where(m => (DateTime.Now.Year - m.DataNascimento.Year) >= idadeMinima && (DateTime.Now.Year - m.DataNascimento.Year) <= idadeMaxima).ToList();
-            foreach (var medico in medicosEntreIdades){
-                Console.WriteLine($"Médico: {medico.Nome}, Idade: {DateTime.Now.Year - medico.DataNascimento.Year}");
-            }
-        }
-        void RelatorioPacientesEntreIdades(List<Paciente> pacientes, int idadeMinima, int idadeMaxima){
-            var pacientesEntreIdades = pacientes.Where(m => (DateTime.Now.Year - m.DataNascimento.Year) >= idadeMinima && (DateTime.Now.Year - m.DataNascimento.Year) <= idadeMaxima).ToList();
-            foreach (var paciente in pacientesEntreIdades) {
-                Console.WriteLine($"Paciente: {paciente.Nome}, Idade: {DateTime.Now.Year - paciente.DataNascimento.Year}");
-            }
-        }
-        void RelatorioPacientesPorSexo(List<Paciente> pacientes, string sexo) {
-            var pacientesPorSexo = pacientes.Where(p => p.Sexo.ToLower() == sexo.ToLower()).ToList();
-            foreach (var paciente in pacientesPorSexo) {
-                Console.WriteLine($"Paciente: {paciente.Nome}, Sexo: {paciente.Sexo}");
-            }
-        }
+    static bool ValidarCPFPaciente(int cpf)
+    {
+        return !pacientes.Any(p => p.CPF == cpf);
+    }
 
-        void RelatorioPacientesOrdemAlfabetica(List<Paciente> pacientes){
-            var pacientesOrdenados = pacientes.OrderBy(p => p.Nome).ToList();
-            foreach (var paciente in pacientesOrdenados) {
-                Console.WriteLine($"Paciente: {paciente.Nome}");
-            }
+    static void AdicionarMedico(string nome, string dataNascimento, int cpf, string crm)
+    {
+        if (ValidarCPFMedico(cpf))
+        {
+            medicos.Add(new Medico
+            {
+                Nome = nome,
+                DataNascimento = dataNascimento,
+                CPF = cpf,
+                CRM = crm
+            });
+            Console.WriteLine("Médico adicionado com sucesso!");
         }
+        else
+        {
+            throw new Exception("CPF já cadastrado para outro médico.");
+        }
+    }
 
-        void RelatorioPacientesPorSintomas(List<Paciente> pacientes, string textoSintomas) {
-            var pacientesComSintomas = pacientes.Where(p => p.Sintomas.ToLower().Contains(textoSintomas.ToLower())).ToList();
-            foreach (var paciente in pacientesComSintomas) {
-                Console.WriteLine($"Paciente: {paciente.Nome}, Sintomas: {paciente.Sintomas}");
-            }
+    static void AdicionarPaciente(string nome, string datadenasc, int cpf, string sexo, string sintomas)
+    {
+        if (ValidarCPFPaciente(cpf))
+        {
+            pacientes.Add(new Paciente
+            {
+                Nome = nome,
+                DataNascimento = datadenasc,
+                CPF = cpf,
+                Sexo = sexo,
+                Sintomas = sintomas
+            });
+            Console.WriteLine("Paciente adicionado com sucesso!");
         }
-        void RelatorioAniversariantesDoMes(int mes, List<Medico> medicos, List<Paciente> pacientes) {
-            var medicosAniversariantes = medicos.Where(m => int.Parse(m.DataNascimento.Split('/')[1]) == mes).ToList();
-            var pacientesAniversariantes = pacientes.Where(p => int.Parse(p.DataNascimento.Split('/')[1]) == mes).ToList();
-            Console.WriteLine($"Aniversariantes do Mês {mes}:");
-            foreach (var medico in medicosAniversariantes) {
-                Console.WriteLine($"Médico aniversariante: {medico.Nome}");
-            }
-            foreach (var paciente in pacientesAniversariantes) {
-                Console.WriteLine($"Paciente aniversariante: {paciente.Nome}");
-            }
+        else
+        {
+            throw new Exception("CPF já cadastrado para outro paciente.");
         }
-        static void MenuPrincipal() {
+    }
+
+    static void RelatorioMedicosEntreIdades(List<Medico> medicos, int idadeMinima, int idadeMaxima)
+    {
+        var medicosEntreIdades = medicos.Where(m => (DateTime.Now.Year - int.Parse(m.DataNascimento.Split('/')[0])) >= idadeMinima && (DateTime.Now.Year - int.Parse(m.DataNascimento.Split('/')[0])) <= idadeMaxima).ToList();
+        foreach (var medico in medicosEntreIdades)
+        {
+            Console.WriteLine($"Médico: {medico.Nome}, Idade: {DateTime.Now.Year - int.Parse(medico.DataNascimento.Split('/')[0])}");
+        }
+    }
+
+    static void RelatorioPacientesPorSexo(List<Paciente> pacientes, string sexo)
+    {
+        var pacientesPorSexo = pacientes.Where(p => p.Sexo.ToLower() == sexo.ToLower()).ToList();
+        foreach (var paciente in pacientesPorSexo)
+        {
+            Console.WriteLine($"Paciente: {paciente.Nome}, Sexo: {paciente.Sexo}");
+        }
+    }
+
+    static void RelatorioPacientesOrdemAlfabetica()
+    {
+        var pacientesOrdenados = pacientes.OrderBy(p => p.Nome).ToList();
+        foreach (var paciente in pacientesOrdenados)
+        {
+            Console.WriteLine($"Paciente: {paciente.Nome}");
+        }
+    }
+
+    static void RelatorioPacientesPorSintomas(List<Paciente> pacientes, string textoSintomas)
+    {
+        var pacientesComSintomas = pacientes.Where(p => p.Sintomas.ToLower().Contains(textoSintomas.ToLower())).ToList();
+        foreach (var paciente in pacientesComSintomas)
+        {
+            Console.WriteLine($"Paciente: {paciente.Nome}, Sintomas: {paciente.Sintomas}");
+        }
+    }
+
+    static void RelatorioAniversariantesDoMes(int mes, List<Medico> medicos, List<Paciente> pacientes)
+{
+    var medicosAniversariantes = medicos.Where(m => int.Parse(m.DataNascimento.Split('/')[1]) == mes).ToList();
+    var pacientesAniversariantes = pacientes.Where(p => int.Parse(p.DataNascimento.Split('/')[1]) == mes).ToList();
+
+    Console.WriteLine($"Aniversariantes do Mês {mes}:");
+
+    foreach (var medico in medicosAniversariantes)
+    {
+        Console.WriteLine($"Médico aniversariante: {medico.Nome}");
+    }
+
+    foreach (var paciente in pacientesAniversariantes)
+    {
+        Console.WriteLine($"Paciente aniversariante: {paciente.Nome}");
+    }
+}
+
+    static void MenuPrincipal()
+    {
         while (true)
         {
             Console.WriteLine("\n=== Menu Principal ===");
@@ -115,23 +144,28 @@ public class Program {
 
             Console.Write("\nEscolha uma opção: ");
             string escolha = Console.ReadLine();
-            switch (escolha) {
+
+            switch (escolha)
+            {
                 case "1":
                     Console.Write("Nome do Médico: ");
                     string nomeMedico = Console.ReadLine();
 
                     Console.Write("Data de Nascimento (yyyy/mm/dd): ");
-                    DateTime dataNascimentoMedico = DateTime.Parse(Console.ReadLine());
+                    string dataNascimentoMedico = Console.ReadLine();
 
                     Console.Write("CPF do Médico: ");
-                    string cpfMedico = Console.ReadLine();
+                    int cpfMedico = int.Parse(Console.ReadLine());
 
                     Console.Write("CRM do Médico: ");
                     string crmMedico = Console.ReadLine();
-                    try {
+
+                    try
+                    {
                         AdicionarMedico(nomeMedico, dataNascimentoMedico, cpfMedico, crmMedico);
                     }
-                    catch (Exception ex) {
+                    catch (Exception ex)
+                    {
                         Console.WriteLine($"Erro ao adicionar médico: {ex.Message}");
                     }
                     break;
@@ -152,10 +186,12 @@ public class Program {
                     Console.Write("Sintomas do Paciente: ");
                     string sintomasPaciente = Console.ReadLine();
 
-                    try {
+                    try
+                    {
                         AdicionarPaciente(nomePaciente, dataNascimentoPaciente, cpfPaciente, sexoPaciente, sintomasPaciente);
                     }
-                    catch (Exception ex) {
+                    catch (Exception ex)
+                    {
                         Console.WriteLine($"Erro ao adicionar paciente: {ex.Message}");
                     }
                     break;
@@ -202,8 +238,10 @@ public class Program {
             }
         }
     }
-                
+
+    static void Main()
+    {
+        MenuPrincipal();
     }
 }
-
-#endregion
+#endregion 
